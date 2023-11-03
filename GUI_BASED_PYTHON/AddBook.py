5686a696192e1e49f18672001fc05d78586d2444
Om Vats <144024782+OMVATS0711@users.noreply.github.com>
2023-11-03 22:13:58 +0530

    Update AddBook.py

--- GUI_BASED_PYTHON/AddBook.py
+++ GUI_BASED_PYTHON/AddBook.py
@@ -2,4 +2,28 @@ from tkinter import *
 from PIL import ImageTk,Image
 from tkinter import messagebox
 import pymysql
- root.mainloop()
+
+def bookRegister():
+    
+    bid = bookInfo1.get()
+    title = bookInfo2.get()
+    author = bookInfo3.get()
+    status = bookInfo4.get()
+    status = status.lower()
+    
+    insertBooks = "insert into "+bookTable+" values('"+bid+"','"+title+"','"+author+"','"+status+"')"
+    try:
+        cur.execute(insertBooks)
+        con.commit()
+        messagebox.showinfo('Success',"Book added successfully")
+    except:
+        messagebox.showinfo("Error","Can't add data into Database")
+    
+    print(bid)
+    print(title)
+    print(author)
+    print(status)
+
+
+    root.destroy()
+    root.mainloop()
