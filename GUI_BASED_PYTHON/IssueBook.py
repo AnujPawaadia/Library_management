82d42235119f5160e6dacd230bc7126cf4131d28
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-11-03 11:32:53 +0530

    Update IssueBook.py

--- GUI_BASED_PYTHON/IssueBook.py
+++ GUI_BASED_PYTHON/IssueBook.py
@@ -12,5 +12,21 @@ cur = con.cursor()
 
 # Enter Table Names here
 issueTable = "books_issued" 
-bookTable = "books" #Book Table
+bookTable = "books"
+    
+#List To store all Book IDs
+allBid = [] 
+
+def issue():
+    
+    global issueBtn,labelFrame,lb1,inf1,inf2,quitBtn,root,Canvas1,status
+    
+    bid = inf1.get()
+    issueto = inf2.get()
+
+    issueBtn.destroy()
+    labelFrame.destroy()
+    lb1.destroy()
+    inf1.destroy()
+    inf2.destroy()
 root.mainloop()
