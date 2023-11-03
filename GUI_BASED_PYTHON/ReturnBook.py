ff0cb7e3b219626385c9a7e6f204a9fb1a57ecc5
Akshat Pandey <akshatpandey26.2004@gmail.com>
2023-11-03 22:55:08 +0530

    Added a Return Function

--- GUI_BASED_PYTHON/ReturnBook.py
+++ GUI_BASED_PYTHON/ReturnBook.py
@@ -16,4 +16,35 @@ bookTable = "books" #Book Table
 
 
 allBid = [] #List To store all Book IDs
+
+def returnn():
+    
+    global SubmitBtn,labelFrame,lb1,bookInfo1,quitBtn,root,Canvas1,status
+    
+    bid = bookInfo1.get()
+
+    extractBid = "select bid from "+issueTable
+    try:
+        cur.execute(extractBid)
+        con.commit()
+        for i in cur:
+            allBid.append(i[0])
+        
+        if bid in allBid:
+            checkAvail = "select status from "+bookTable+" where bid = '"+bid+"'"
+            cur.execute(checkAvail)
+            con.commit()
+            for i in cur:
+                check = i[0]
+                
+            if check == 'issued':
+                status = True
+            else:
+                status = False
+
+        else:
+            messagebox.showinfo("Error","Book ID not present")
+    except:
+        messagebox.showinfo("Error","Can't fetch Book IDs")
+
 root.mainloop()
