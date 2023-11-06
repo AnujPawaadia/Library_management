204876600b0c0ec5f70c1362924f491a32e212fb
Anuj Pawadia <124757784+AnujPawaadia@users.noreply.github.com>
2023-11-06 23:31:29 +0530

    Update DeleteBook.py

--- GUI_BASED_PYTHON/DeleteBook.py
+++ GUI_BASED_PYTHON/DeleteBook.py
@@ -63,10 +63,6 @@ def delete():
     quitBtn = Button(root,text="Quit",bg='#f7f1e3', fg='black', command=root.destroy)
     quitBtn.place(relx=0.53,rely=0.9, relwidth=0.18,relheight=0.08)
     
-    print(bid)
-    print(title)
-    print(author)
-    print(status)
-
+   
 
     root.destroy()
