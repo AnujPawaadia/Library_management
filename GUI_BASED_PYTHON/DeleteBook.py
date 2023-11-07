5dca98e08ccf140f19450ccc8b6c35abcd0fb09e
Anshul_Mantri <144048433+anshulmantri2005@users.noreply.github.com>
2023-11-07 09:10:41 +0530

    Update DeleteBook.py

--- GUI_BASED_PYTHON/DeleteBook.py
+++ GUI_BASED_PYTHON/DeleteBook.py
@@ -50,19 +50,17 @@ def delete():
 
 
     # Book ID to Delete
-    lb2 = Label(labelFrame,text="Book ID : ", bg='black', fg='white')
-    lb2.place(relx=0.05,rely=0.5)
+   7 lb2 = Label(labelFrame,text="BOOK ID : ", bg='black', fg='white')
+    8lb2.place(relx=0.05,rely=0.5)
         
-    bookInfo1 = Entry(labelFrame)
+  9  bookInfo1 = Entry(labelFrame)
     bookInfo1.place(relx=0.3,rely=0.5, relwidth=0.62)
     
     #Submit Button
-    SubmitBtn = Button(root,text="SUBMIT",bg='#d1ccc0', fg='black',command=deleteBook)
+   0 SubmitBtn = Button(root,text="SUBMIT",bg='#d1ccc0', fg='black',command=deleteBook)
     SubmitBtn.place(relx=0.28,rely=0.9, relwidth=0.18,relheight=0.08)
     
     quitBtn = Button(root,text="Quit",bg='#f7f1e3', fg='black', command=root.destroy)
     quitBtn.place(relx=0.53,rely=0.9, relwidth=0.18,relheight=0.08)
     
-   
-
-    root.destroy()
+    root.mainloop()
