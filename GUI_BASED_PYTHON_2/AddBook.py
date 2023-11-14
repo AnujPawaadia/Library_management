fe223883683c799d80f8b30508df14c1ec29e61f
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-11-14 16:08:04 +0530

    Update AddBook.py

--- GUI_BASED_PYTHON_2/AddBook.py
+++ GUI_BASED_PYTHON_2/AddBook.py
@@ -3,6 +3,62 @@ from PIL import ImageTk,Image
 from tkinter import messagebox
 import pymysql
 
+def bookRegister():
+    
+    bid = bookInfo1.get()
+    title = bookInfo2.get()
+    author = bookInfo3.get()
+    status = bookInfo4.get()
+    status = status.lower()
+    
+    insertBooks = "insert into "+bookTable+" values('"+bid+"','"+title+"','"+author+"','"+status+"')"
+    try:
+        cur.execute(insertBooks)
+        con.commit()
+        messagebox.showinfo('Success',"Book added successfully")
+    except:
+        messagebox.showinfo("Error","Can't add data into Database")
+    
+    print(bid)
+    print(title)
+    print(author)
+    print(status)
 
+
+    root.destroy()
+    
+def addBook(): 
+    
+    global bookInfo1,bookInfo2,bookInfo3,bookInfo4,Canvas1,con,cur,bookTable,root
+    
+    root = Tk()
+    root.title("Library")
+    root.minsize(width=400,height=400)
+    root.geometry("600x500")
+
+    # Add your own database name and password here to reflect in the code
+    mypass = "root"
+    mydatabase="db"
+
+    con = pymysql.connect(host="localhost",user="root",password="1234",database=mydatabase)
+    cur = con.cursor()
+
+    # Enter Table Names here
+    bookTable = "books" # Book Table
+
+    Canvas1 = Canvas(root)
+    
+    Canvas1.config(bg="#ff6e40")
+    Canvas1.pack(expand=True,fill=BOTH)
+        
+    headingFrame1 = Frame(root,bg="#FFBB00",bd=5)
+    headingFrame1.place(relx=0.25,rely=0.1,relwidth=0.5,relheight=0.13)
+
+    headingLabel = Label(headingFrame1, text="ADD BOOKS", bg='black', fg='white', font=('Courier', 20, 'bold'))
+    headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
+
+
+    labelFrame = Frame(root,bg='black')
+    labelFrame.place(relx=0.1,rely=0.4,relwidth=0.8,relheight=0.4
     
     root.mainloop()
