6b5fdb8440e2b3570994c213e9c514a2fd347407
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-11-17 11:55:31 +0530

    Create ReturnBook.py

---
+++ GUI_BASED_PYTHON_2/ReturnBook.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from PIL import ImageTk,Image
+from tkinter import messagebox
+import pymysql
+
+# Add your own database name and password here to reflect in the code
+mypass = "root"
+mydatabase="db"
+
+con = pymysql.connect(host="localhost",user="root",password="1234",database=mydatabase)
+cur = con.cursor()
+
+# Enter Table Names here
+issueTable = "books_issued" #Issue Table
+bookTable = "books" #Book Table
+
+
+allBid = [] #List To store all Book ID
+    
+    root.mainloop()
